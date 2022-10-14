61c4a50b136648be5fba535196f4d2d6dbfa5cd3
damhiya <damhiya@gmail.com>
2022-10-15 06:36:45 +0900

    home-manager import

--- users/damhiya/default.nix
+++ users/damhiya/default.nix
@@ -1,21 +1,19 @@
-let
-  modules = [
-    ./bash.nix
-    ./git.nix
-    ./emacs.nix
-    ./termite.nix
-    ./direnv.nix
-    ./ghc.nix
-    ./packages.nix
-    ./programs.nix
-    ./kime.nix
-    ./xmonad.nix
-    ./polybar.nix
-  ];
-in { pkgs, config, ... }@args: {
-  imports = map (m: { home-manager.users.damhiya = import m args; }) modules;
-
+{ pkgs, config, ... }@args: {
   home-manager.users.damhiya = {
+    imports = [
+      ./bash.nix
+      ./git.nix
+      ./emacs.nix
+      ./termite.nix
+      ./direnv.nix
+      ./ghc.nix
+      ./packages.nix
+      ./programs.nix
+      ./kime.nix
+      ./xmonad.nix
+      ./polybar.nix
+    ];
+
     home.username = "damhiya";
     home.homeDirectory = "/home/damhiya";
     home.stateVersion = "22.05";
