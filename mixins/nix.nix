066734d05361d2aa83ada424548ddd88e46fc2ea
damhiya <damhiya@gmail.com>
2022-12-06 16:04:21 +0900

    replace hydra.iohk.io with cache.iog.io

--- mixins/nix.nix
+++ mixins/nix.nix
@@ -12,7 +12,7 @@
         "https://nixpkgs-wayland.cachix.org"
       ];
       trusted-substituters = [
-        "https://hydra.iohk.io"
+        "https://cache.iog.io"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
