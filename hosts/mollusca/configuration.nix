72eee88f4d4e89f84c9e580f52bcd1c287d7190c
damhiya <damhiya@gmail.com>
2023-03-11 14:26:28 +0900

    fix whitespace

--- hosts/mollusca/configuration.nix
+++ hosts/mollusca/configuration.nix
@@ -138,21 +138,21 @@ in {
       config = ''
         (defsrc
             esc  f1   f2   f3   f4   f5   f6   f7   f8   f9   f10  f11  f12  home end  ins  del
-            grv  1    2    3    4    5    6    7    8    9    0    -     =    bspc
-            tab  q    w    e    r    t    y    u    i    o    p    [     ]    \
-            caps a    s    d    f    g    h    j    k    l    ;    '          ret
-            lsft z    x    c    v    b    n    m    ,    .    /               rsft
-                 lctl lmet lalt           spc            ralt sys  rctl  pgdn up   pgup
-                                                                         left down rght
+            grv  1    2    3    4    5    6    7    8    9    0    -    =    bspc
+            tab  q    w    e    r    t    y    u    i    o    p    [    ]    \
+            caps a    s    d    f    g    h    j    k    l    ;    '         ret
+            lsft z    x    c    v    b    n    m    ,    .    /              rsft
+                 lctl lmet lalt           spc            ralt sys  rctl pgdn up   pgup
+                                                                        left down rght
         )
         (deflayer qwerty
             esc  f1   f2   f3   f4   f5   f6   f7   f8   f9   f10  f11  f12  home end  ins  del
-            grv  1    2    3    4    5    6    7    8    9    0    -     =    bspc
-            tab  q    w    e    r    t    y    u    i    o    p    [     ]    \
-            lctl a    s    d    f    g    h    j    k    l    ;    '          ret
-            lsft z    x    c    v    b    n    m    ,    .    /               rsft
-                 lctl lmet lalt           spc            ralt sys  rctl  pgdn up   pgup
-                                                                         left down rght
+            grv  1    2    3    4    5    6    7    8    9    0    -    =    bspc
+            tab  q    w    e    r    t    y    u    i    o    p    [    ]    \
+            lctl a    s    d    f    g    h    j    k    l    ;    '         ret
+            lsft z    x    c    v    b    n    m    ,    .    /              rsft
+                 lctl lmet lalt           spc            ralt sys  rctl pgdn up   pgup
+                                                                        left down rght
         )
       '';
     };
