42dcc51d9351969165e4e011962b41d3c5627cd7
damhiya <damhiya@gmail.com>
2023-05-12 19:17:33 +0900

    update icon

--- users/damhiya/x11/polybar.nix
+++ users/damhiya/x11/polybar.nix
@@ -37,7 +37,7 @@
         label-full = "  Full %consumption%W";
         label-charging = "%percentage%% %consumption%W";
         label-discharging = "%percentage%% %consumption%W";
-        animation-charging = [ "" "" "" "" "" ];
+        animation-charging = [ "" "" "" "" "" ]; # nf-fa-battery
         animation-charging-framerate = 750;
         ramp-capacity = [ "" "" "" "" "" ];
       };
@@ -46,8 +46,8 @@
         interface = "wlp0s20f3";
         format-connected = "<label-connected>";
         format-disconnected = "<label-disconnected>";
-        label-connected = "直  %essid%  %upspeed%  %downspeed%";
-        label-disconnected = "睊  Offline";
+        label-connected = "󰖩  %essid% 󰁞 %upspeed% 󰁆 %downspeed%"; # nf-md-wifi, nf-md-arrow_up_thick, nf-md-arrow_down_thick
+        label-disconnected = "󰖪  Offline"; # nf-md-wifi_off
       };
     };
     script = ''
