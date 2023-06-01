561d6307c304c3914114ddec88ffbd726d0598f4
damhiya <damhiya@gmail.com>
2023-06-02 03:33:57 +0900

    add xwininfo & autorandr

--- users/damhiya/x11/programs.nix
+++ users/damhiya/x11/programs.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }: {
   programs.feh.enable = true;
   programs.rofi.enable = true;
-  home.packages = with pkgs; [ xclip xcwd arandr ];
+  home.packages = with pkgs; [ xorg.xwininfo autorandr xclip xcwd arandr ];
 }
