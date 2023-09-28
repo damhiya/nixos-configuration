56bbc6144d7a440940e010d26a220f2f48acccb5
damhiya <damhiya@gmail.com>
2023-09-28 14:42:18 +0900

    add teams to mimeApps

--- users/damhiya/mimeApps.nix
+++ users/damhiya/mimeApps.nix
@@ -24,6 +24,9 @@
       "image/jpeg" = "org.nomacs.ImageLounge.desktop";
       "image/gif" = "org.nomacs.ImageLounge.desktop";
       "image/tiff" = "org.nomacs.ImageLounge.desktop";
+
+      # ms teams
+      "x-scheme-handler/msteams" = "teams.desktop";
     };
   };
 }
