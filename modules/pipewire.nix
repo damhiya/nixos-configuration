605eaeb494a1b72ed07956662ddb400789bcf382
damhiya <damhiya@gmail.com>
2023-10-31 00:07:48 +0900

    piprewire config

--- modules/pipewire.nix
+++ modules/pipewire.nix
@@ -5,9 +5,12 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
+    wireplumber.enable = true;
   };
 
   environment.systemPackages = with pkgs; [
+    # See https://www.reddit.com/r/NixOS/comments/wyu3rj/how_to_get_pactl_in_pipewire/
+    pulseaudio
     pulsemixer
     pamixer
   ];
