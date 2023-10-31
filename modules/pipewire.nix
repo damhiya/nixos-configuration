3cf1943143bfa08df2f421cf3977a4125e0b1414
damhiya <damhiya@gmail.com>
2023-11-01 03:03:56 +0900

    alsamixer utility

--- modules/pipewire.nix
+++ modules/pipewire.nix
@@ -10,6 +10,8 @@
 
   environment.systemPackages = with pkgs; [
     # See https://www.reddit.com/r/NixOS/comments/wyu3rj/how_to_get_pactl_in_pipewire/
+    # https://unix.stackexchange.com/questions/36937/volume-on-linux-much-lower-than-on-windows
+    alsa-utils
     pulseaudio
     pulsemixer
     pamixer
