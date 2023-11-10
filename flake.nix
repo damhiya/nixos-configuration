d1c9e8992a4fb9a199a83cb6d7ed028c187d9b33
damhiya <damhiya@gmail.com>
2023-11-11 03:50:30 +0900

    remove nixpkgs-wayland

--- flake.nix
+++ flake.nix
@@ -5,13 +5,11 @@
     substituters = [
       "https://cache.nixos.org/"
       "https://nix-community.cachix.org"
-      "https://nixpkgs-wayland.cachix.org"
       "https://hyprland.cachix.org"
     ];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
     ];
   };
@@ -20,8 +18,6 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    nixpkgs-wayland.url = "github:nix-community/nixpkgs-wayland";
-    nixpkgs-wayland.inputs.nixpkgs.follows = "nixpkgs";
     kmonad.url = "github:kmonad/kmonad?dir=nix";
     kmonad.inputs.nixpkgs.follows = "nixpkgs";
     iosevka-custom.url = "github:damhiya/iosevka-custom";
@@ -30,8 +26,8 @@
     hhg-overlay.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, home-manager, nixpkgs-wayland, kmonad
-    , iosevka-custom, notsodeep-overlay, hhg-overlay }:
+  outputs = { self, nixpkgs, home-manager, kmonad, iosevka-custom
+    , notsodeep-overlay, hhg-overlay }:
     let
       baseModule = {
         imports =
@@ -39,7 +35,6 @@
         system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
         nix.registry.nixos.flake = nixpkgs;
         nixpkgs.overlays = [
-          nixpkgs-wayland.overlays.default
           iosevka-custom.overlays.default
           notsodeep-overlay.overlays.default
           hhg-overlay.overlays.default
