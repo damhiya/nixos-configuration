c7dc1c0446cac82e3ced735bdc758237790c4a90
damhiya <damhiya@gmail.com>
2024-03-11 00:36:02 +0900

    refactoring kmonad

--- hosts/mollusca/configuration.nix
+++ hosts/mollusca/configuration.nix
@@ -113,34 +113,36 @@
 
   services.kmonad = {
     enable = true;
-    keyboards.thinkpad-internal = {
-      device = "/dev/input/by-path/platform-i8042-serio-0-event-kbd";
-      defcfg = {
-        enable = true;
-        compose.key = null;
-        fallthrough = true;
-        allowCommands = false;
+    keyboards = {
+      thinkpad-internal = {
+        device = "/dev/input/by-path/platform-i8042-serio-0-event-kbd";
+        defcfg = {
+          enable = true;
+          compose.key = null;
+          fallthrough = true;
+          allowCommands = false;
+        };
+        config = ''
+          (defsrc
+              esc  f1   f2   f3   f4   f5   f6   f7   f8   f9   f10  f11  f12  home end  ins  del
+              grv  1    2    3    4    5    6    7    8    9    0    -    =    bspc
+              tab  q    w    e    r    t    y    u    i    o    p    [    ]    \
+              caps a    s    d    f    g    h    j    k    l    ;    '         ret
+              lsft z    x    c    v    b    n    m    ,    .    /              rsft
+                   lctl lmet lalt           spc            ralt sys  rctl pgdn up   pgup
+                                                                          left down rght
+          )
+          (deflayer qwerty
+              esc  f1   f2   f3   f4   f5   f6   f7   f8   f9   f10  f11  f12  home end  ins  del
+              grv  1    2    3    4    5    6    7    8    9    0    -    =    bspc
+              tab  q    w    e    r    t    y    u    i    o    p    [    ]    \
+              lctl a    s    d    f    g    h    j    k    l    ;    '         ret
+              lsft z    x    c    v    b    n    m    ,    .    /              rsft
+                   lctl lalt lmet           spc            ralt sys  rctl pgdn up   pgup
+                                                                          left down rght
+          )
+        '';
       };
-      config = ''
-        (defsrc
-            esc  f1   f2   f3   f4   f5   f6   f7   f8   f9   f10  f11  f12  home end  ins  del
-            grv  1    2    3    4    5    6    7    8    9    0    -    =    bspc
-            tab  q    w    e    r    t    y    u    i    o    p    [    ]    \
-            caps a    s    d    f    g    h    j    k    l    ;    '         ret
-            lsft z    x    c    v    b    n    m    ,    .    /              rsft
-                 lctl lmet lalt           spc            ralt sys  rctl pgdn up   pgup
-                                                                        left down rght
-        )
-        (deflayer qwerty
-            esc  f1   f2   f3   f4   f5   f6   f7   f8   f9   f10  f11  f12  home end  ins  del
-            grv  1    2    3    4    5    6    7    8    9    0    -    =    bspc
-            tab  q    w    e    r    t    y    u    i    o    p    [    ]    \
-            lctl a    s    d    f    g    h    j    k    l    ;    '         ret
-            lsft z    x    c    v    b    n    m    ,    .    /              rsft
-                 lctl lalt lmet           spc            ralt sys  rctl pgdn up   pgup
-                                                                        left down rght
-        )
-      '';
     };
   };
 
