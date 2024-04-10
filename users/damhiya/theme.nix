f2515fb42466939316fb65c71ee1143a4e596066
damhiya <damhiya@gmail.com>
2024-04-10 15:40:51 +0900

    gtk/qt theme

--- users/damhiya/theme.nix
+++ users/damhiya/theme.nix
@@ -14,10 +14,26 @@
       size = 10;
     };
     gtk2.configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
+    theme = {
+      package = pkgs.kdePackages.breeze-gtk;
+      name = "breeze";
+    };
+    iconTheme = {
+      package = pkgs.kdePackages.breeze-gtk;
+      name = "breeze";
+    };
   };
 
   qt = {
     enable = true;
-    platformTheme = "gtk3";
+    platformTheme = "kde";
+    style = {
+      package = pkgs.kdePackages.breeze;
+      name = "breeze";
+    };
   };
+
+  home.packages = with pkgs; [
+    kdePackages.breeze-icons
+  ];
 }
