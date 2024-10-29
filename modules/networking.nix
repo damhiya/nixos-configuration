40813cd716a68ae8ee9ee8da3f31c430be41eaa0
damhiya <damhiya@gmail.com>
2024-10-29 16:05:28 +0900

    disable notsodeep

--- modules/networking.nix
+++ modules/networking.nix
@@ -1,5 +1,4 @@
 { pkgs, ... }: {
-
   networking = {
     nameservers = [ "1.1.1.1" "1.0.0.1" ];
     wireless.enable = false;
@@ -7,18 +6,6 @@
     firewall = {
       enable = true;
       allowedTCPPorts = [ 80 443 ];
-      extraCommands = ''
-        iptables -A INPUT -p tcp --tcp-flags SYN,ACK SYN,ACK --sport 443 -j NFQUEUE --queue-num 200 --queue-bypass
-        iptables -t raw -I PREROUTING -p tcp --sport 443 --tcp-flags SYN,ACK SYN,ACK -j NFQUEUE --queue-num 200 --queue-bypass
-        iptables -t raw -I PREROUTING -p tcp --sport 80 --tcp-flags SYN,ACK SYN,ACK -j NFQUEUE --queue-num 200 --queue-bypass
-      '';
     };
   };
-
-  systemd.services.notsodeep = {
-    description = "notsodeep";
-    serviceConfig = { ExecStart = "${pkgs.notsodeep}/bin/notsodeep"; };
-    wantedBy = [ "multi-user.target" ];
-  };
-
 }
