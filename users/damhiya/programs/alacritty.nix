afaacbc061b81c7a779a863679315624e84ace3c
damhiya <damhiya@gmail.com>
2025-01-12 22:08:30 +0900

    update alacritty config

--- users/damhiya/programs/alacritty.nix
+++ users/damhiya/programs/alacritty.nix
@@ -3,7 +3,7 @@
   programs.alacritty = {
     enable = true;
     settings = {
-      shell = "${pkgs.fish}/bin/fish";
+      terminal.shell = "${pkgs.fish}/bin/fish";
       cursor.style.blinking = "Always";
       font = {
         normal.family = "Iosevka Fixed";
