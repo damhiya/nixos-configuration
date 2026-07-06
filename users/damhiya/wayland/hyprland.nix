ce18c2b9f3e554c7668c70f2da9151611e5724fb
damhiya <damhiya@gmail.com>
2026-07-06 12:50:53 +0900

    remove old comments

--- users/damhiya/wayland/hyprland.nix
+++ users/damhiya/wayland/hyprland.nix
@@ -38,10 +38,6 @@
           size = 10;
           passes = 3;
         };
-        # drop_shadow = true;
-        # shadow_range = 4;
-        # shadow_render_power = 3;
-        # "col.shadow" = "rgba(1a1a1aee)";
       };
       misc = {
         force_default_wallpaper = 2;
@@ -62,7 +58,6 @@
         ];
       };
       dwindle.preserve_split = true;
-      # gestures.workspace_swipe = true;
 
       "$mainMod" = "SUPER";
       bind = [
