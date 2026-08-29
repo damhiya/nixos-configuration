7e8133746ca149110cfcc738f01367d92bd84f9d
damhiya <damhiya@gmail.com>
2026-08-30 00:00:57 +0900

    update nix substituters

--- modules/nix.nix
+++ modules/nix.nix
@@ -12,13 +12,9 @@
       ];
       trusted-substituters = [
         "https://nix-community.cachix.org"
-        "https://nixpkgs-wayland.cachix.org"
-        "https://cache.iog.io"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
-        "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
       ];
     };
   };
